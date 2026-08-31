1a82d26d009be54d974531c67935f406162d8ef6
Nicolas Koron <nkoron@itba.edu.ar>
2026-08-31 17:06:54 -0300

    chore: add manual API smoke test

--- src/main/java/edu/itba/class1/exchange/main/Main.java
+++ src/main/java/edu/itba/class1/exchange/main/Main.java
@@ -1,18 +1,25 @@
 import edu.itba.class1.exchange.client.currencyapi.CurrencyApiProvider;
 import edu.itba.class1.exchange.client.currencyapi.FreeCurrencyApiClient;
 import edu.itba.class1.exchange.http.UnirestHttpClient;
-import edu.itba.class1.exchange.model.MoneyAmount;
 import edu.itba.class1.exchange.parser.GsonJsonParser;
-import edu.itba.class1.exchange.service.CurrencyConverter;
 
-import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.List;
+import java.time.LocalDate;
 
 void main() {
-	final var httpClient = new UnirestHttpClient();
-	final var parser = new GsonJsonParser();
-	final var currencyApiClient = new FreeCurrencyApiClient(httpClient, parser);
-	final var provider = new CurrencyApiProvider(currencyApiClient);
-	final var converter = new CurrencyConverter(provider);
-	final var ars = new MoneyAmount(Currency.getInstance("ARS"), BigDecimal.valueOf(100.0));
+	var httpClient = new UnirestHttpClient();
+	var parser = new GsonJsonParser();
+	var client = new FreeCurrencyApiClient(httpClient, parser);
+	var provider = new CurrencyApiProvider(client);
+
+	System.out.println(provider.getAvailableCurrencies());
+	System.out.println(provider.getMultipleCurrencyRates(
+			Currency.getInstance("USD"),
+			List.of(Currency.getInstance("EUR"), Currency.getInstance("JPY"))));
+
+	System.out.println(provider.getHistoricalMultipleCurrencyRates(
+			Currency.getInstance("USD"),
+			List.of(Currency.getInstance("EUR"), Currency.getInstance("JPY")),
+			LocalDate.of(2024, 11, 20)));
 }
